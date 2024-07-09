331818415e496521a99aa3eeab7f69262a06e399
Ballard <ballard@button.is>
2024-07-09 18:47:33 -0400

    added MUI paaper component to item.js

--- frontend/src/components/Item.js
+++ frontend/src/components/Item.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Paper, Typography } from '@mui/material';
 
-const Item = ({name, body}) => {
-    return (
-        <div className='item'>
-            <h3>{name}</h3>
-            <p>{body}</p>
-        </div>
-    );
+const Item = ({ name, body }) => {
+  return (
+    <Paper elevation={3} sx={{ padding: 2, margin: 1 }}>
+      <Typography variant="h6" component="h3">
+        {name}
+      </Typography>
+      <Typography variant="body2">
+        {body}
+      </Typography>
+    </Paper>
+  );
 };
+
 export default Item;
