6e64f508e30eb1c1e7270469f09559a792246505
Tai Robinett <tairobinett@gmail.com>
2024-07-30 20:19:58 -0400

    Switched from Flask to fastapi. Made hello world route.

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,9 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import psycopg2
-import time
-import os
+from fastapi import FastAPI
 
-app = Flask(__name__)
-CORS(app)
+app = FastAPI()
 
-@app.route("/hello")
-def hello_world(): 
-    return "<p>Hello World!</p>"
+@app.get("/hello")
+async def root():
+    return {
+        "message": "Hello world!"
+    }
